8d84f0d03659809b5747fadfe92434898ac4d555
BryanWalace <bryanwalace@hotmail.com>
2025-01-16 02:15:26 -0300

    Fundamentals 1.1
    Fundamentals 1.1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Bryan");
+console.log(23);
+
+let firstName = "Bryan";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let Person = 'Bryan';
+let PI = 3.1415;
