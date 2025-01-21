02aac6afd1a1152972c1c59dcc6f63e184e04149
BryanWalace <bryanwalace@hotmail.com>
2025-01-21 08:52:40 -0300

    Data Types
    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,5 @@
-let js = 'amazing';
+/*
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Bryan");
@@ -9,22 +10,55 @@ let firstName = "Bryan";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-
-let jonas_matilda = 'JM';
+ 
+// Variable name conventions
+let jonas_matilda = "JM";
 let $function = 27;
 
-let Person = 'Bryan';
+let Person = "Bryan";
 let PI = 3.1415;
 
-let myFirstJob = 'Programmer';
-let myCurrentJob = 'Programmer';
+let myFirstJob = "Programmer";
+let myCurrentJob = "Programmer";
 
 console.log(myFirstJob);
 
-let country = 'Brasil';
-let continent = 'South America';
+// Exercise Values and Variables
+let country = "Brasil";
+let continent = "South America";
 let population = 216_400_000;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Bryan");
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2000;
+console.log(typeof year);
+
+console.log(typeof null);
+
+// Exercise Data Types
+let isIsLand = true;
+console.log(typeof isIsLand);
+
+let language;
+console.log(typeof language);
+
+console.log(typeof population);
+console.log(typeof country);
